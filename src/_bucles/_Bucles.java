d338e7e8df262569088c89c3cb29e6a4e9683f69
jonomy <ceciliabelmar12345@gmail.com>
2017-11-03 22:12:11 -0300

    Imprime los numerosdel 0 al 100 mediante un ciclo while

---
+++ src/_bucles/_Bucles.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package _bucles;
+
+/**
+ *
+ * @author LAB06
+ */
+public class _Bucles {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        int stop =-1;
+        int index =0; 
+        while ( index<=100)//Miemtras SE CUMPLA LA CONDICION
+        {
+            //STOP = Interger.parceInt(JOPtionPane.showInputDialog("Ingrese un numero"));
+            System .out .println(index);
+            index++;//Incrementamos la variable en 1
+        }
+        
+        
+        //TODO Code application logic here
+    }
+}
+        }
+        
+    }
+    
+}
